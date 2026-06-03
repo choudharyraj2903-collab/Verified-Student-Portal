d400c633930309c5196ba572f1314142eca0303e
Raj Choudhary <choudharyraj2903@gmail.com>
2026-06-03 12:11:10 +0530

    step 0:basic backend setup with config,db + added gitignore with /keys *.pem .env* .env in it

---
+++ backend/db/connection.go
@@ -0,0 +1,13 @@
+package db 
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module student_portal/backend
+
+go 1.25.6
